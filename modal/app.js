345eec6653b89d6de085484310af990d097e91a8
George <georgecharleschang@gmail.com>
2020-06-01 23:52:52 -0700

    Add close modal when clicking outside functionality

--- modal/app.js
+++ modal/app.js
@@ -16,3 +16,10 @@ button.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   modal.style.display = "none";
 });
+
+// Close modal when the 
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal-overlay")) {
+    modal.style.display = "none";
+  }
+});
